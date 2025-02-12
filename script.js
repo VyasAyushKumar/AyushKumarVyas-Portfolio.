504a56c0ba075fdceb9c0161923e03ac32e8469f
Ayush Kumar <serverpunkz@gmail.com>
2025-02-13 03:41:59 +0530

    hamburger update with scrollTrigger element

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
         smooth: true
     });
     
+  var menu = document.querySelector("nav i");
+  var full = document.querySelector("#full-scr")
+  var navimg = document.querySelector(".nav-right")
+  flag = 0
+  menu.addEventListener("click", function() {
+       if(flag == 0){
+        full.style.top = 0
+        menu.style.scale = 1.3
+        flag = 1
+       }
+       else{
+        full.style.top = "-100vh"
+        menu.style.scale = 1
+        flag = 0
+       }
+  });
